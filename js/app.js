c23632b7eccd707da4a49c2f09b3f20810e25cbb
new dvlpr <patil.rajiv@gmail.com>
2016-04-06 23:41:33 +0530

    homepage changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
+	
+	
 	$("#findBuddyBtn").on('click',function(e){
+		$("#tripsBtn").removeClass("highlights");
 		$("#tripsPage").slideUp("slow");
 		$("#findBuddyPage").slideDown("slow");
 	});
 	
 	$("#tripsBtn").on('click',function(e){
+
+		$("#tripsBtn").addClass("highlight");
 		$("#findBuddyPage").slideUp("slow");
 		$("#tripsPage").slideDown("slow");
 
 	});
 	
+	$("#tripsBtn").on('onLoad',function(e){
+			$("#tripsBtn").addClass("highlights");
+	});
+	
 	$('#datetimepicker1').datetimepicker({showClose:  true,ignoreReadonly: true});
 
+	
+	
+	$("#tripsBtn").trigger("onLoad");
 });
