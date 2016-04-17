971fb56fbc3532f1d23d75dddcc6ef2539c5b2eb
new dvlpr <patil.rajiv@gmail.com>
2016-04-18 00:14:10 +0530

    remove effects

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 	
 	$("#findBuddyBtn").on('click',function(e){
 		$("#tripsBtn").removeClass("highlights");
-		$("#tripsPage").hide("slow");
-		$("#addTrip-step1").hide("slow");
-		$("#upcomingTripsHome").hide("slow");
-		$("#findBuddyPage").show("slow");
+		$("#tripsPage").hide();
+		$("#addTrip-step1").hide();
+		$("#upcomingTripsHome").hide();
+		$("#findBuddyPage").show();
 	});
 
 	$("#tripsBtn").on('click',function(e){
 
 		$("#tripsBtn").addClass("highlight");
-		$("#findBuddyPage").hide("slow");
-		$("#addTrip-step1").hide("slow");
-		$("#upcomingTripsHome").hide("slow");
-		$("#tripsPage").show("slow");
+		$("#findBuddyPage").hide();
+		$("#addTrip-step1").hide();
+		$("#upcomingTripsHome").hide();
+		$("#tripsPage").show();
 
 	});
 	
