999a0d453a21a8801e625f872e6c97ed18bf5625
new dvlpr <patil.rajiv@gmail.com>
2016-04-17 23:57:25 +0530

    updates to trips pages

--- js/app.js
+++ js/app.js
@@ -4,32 +4,49 @@ $(document).ready(function(){
 	$("#findBuddyBtn").on('click',function(e){
 		$("#tripsBtn").removeClass("highlights");
 		$("#tripsPage").slideUp("slow");
+		$("#addTrip-step1").slideUp("slow");
+		$("#upcomingTripsHome").slideUp("slow");
 		$("#findBuddyPage").slideDown("slow");
 	});
-	
+
 	$("#tripsBtn").on('click',function(e){
 
 		$("#tripsBtn").addClass("highlight");
 		$("#findBuddyPage").slideUp("slow");
+		$("#addTrip-step1").slideUp("slow");
+		$("#upcomingTripsHome").slideUp("slow");
 		$("#tripsPage").slideDown("slow");
 
 	});
 	
-	$("#tripsBtn").on('onLoad',function(e){
-			$("#tripsBtn").addClass("highlights");
-	});
-	
-	$('#datetimepicker1').datetimepicker({showClose:  true,ignoreReadonly: true});
-
 	$('#upcmgTripDshbrd').on('click',function(e){
+		$("#tripsBtn").addClass("highlights");
 		$('#tripsPage').hide();
+		$("#findBuddyPage").hide();
+		$("#addTrip-step1").hide();
 		$('#upcomingTripsHome').show();
 	});
 
 	$('#planATripBtn').on('click',function(e){
+		$("#tripsBtn").addClass("highlights");
+		$('#tripsPage').hide();
+		$("#findBuddyPage").hide();
 		$('#upcomingTripsHome').hide();
 		$('#addTrip-step1').show();
 	})
+
+
+	$("#tripsBtn").on('onLoad',function(e){
+			$("#tripsBtn").addClass("highlights");
+	});
 	
+	//$('#datetimepicker1').datetimepicker({showClose:  true,ignoreReadonly: true});
+
+	
+	$('#datetimepicker12').datetimepicker({
+        inline: true,
+        sideBySide: true
+    });
+
 	$("#tripsBtn").trigger("onLoad");
 });
